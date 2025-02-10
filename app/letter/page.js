48fef1290a64dcae2f53e805db0dc7d782fdbbda
SohxmP <141549578+SohxmP@users.noreply.github.com>
2025-02-10 14:37:49 +0530

    Update page.js

--- app/letter/page.js
+++ app/letter/page.js
@@ -13,13 +13,18 @@ export default function Letter(){
     const [rm, setRm] = useState({class: 'rm-hidden'});
     const [sl, setSl] = useState(false);
 
-    useEffect(() => {
+    try{
         window.onscroll = function(ev) {
             if (((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight - 100) && (rm.class == 'rm')) {
                 setSl(true);
             }
         };
-        
+    }
+    catch(e){
+        alert(e);
+    }
+
+    useEffect(() => {
         if(localStorage.getItem('code') == '070524'){
 
         }
